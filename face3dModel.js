23ca0e8cf6e56ed92521e0ebe2f011051bc8be6b
piyushk1024 <piyushk256@gmail.com>
2022-11-03 17:19:29 +0530

    Picture  Capture

--- face3dModel.js
+++ face3dModel.js
@@ -2,6 +2,38 @@ import {loadGLTF} from "./static/libs/loader.js";
 //import {mockWithVideo} from '../../libs/camera-mock';
 const THREE = window.MINDAR.FACE.THREE;
 
+//picture canvas
+const capture = mindarThree => {
+
+  const {video,renderer,scene,camera} = mindarThree;
+  const renderCanvas = renderer.domElement;
+
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.width = renderCanvas.width;
+  canvas.height = renderCanvas.height;
+
+  const sx = (video.clientWidth - renderCanvas.clientWidth) / 2 * video.videoWidth / video.clientWidth;
+  const sy = (video.clientHeight - renderCanvas.clientHeight) / 2 * video.videoHeight / video.clientHeight;
+  const sw = video.videoWidth - sx * 2; 
+  const sh = video.videoHeight - sy * 2; 
+
+  context.drawImage(video,sx,sy,sw,sh,0,0, canvas.width,canvas.height);
+
+  renderer.preserveDrawingBuffer = true;
+  renderer.render(scene,camera);
+  context.drawImage(renderCanvas,0,0,canvas.width,canvas.height);
+  renderer.preserveDrawingBuffer = false;
+
+  const data = canvas.toDataURL('image/png');
+
+  const link = document.createElement('a');
+  link.download = 'photo.png';
+  link.href = data;
+  link.click();
+}
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
@@ -55,6 +87,9 @@ document.addEventListener('DOMContentLoaded', () => {
     anchor.onTargetLost = () => {
       
     }
+    document.querySelector("#capture").addEventListener("click", () => {
+      capture(mindarThree);
+    });
 
     // start AR
     await mindarThree.start();
